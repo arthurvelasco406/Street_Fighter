0fd1c53511ab23d4c05b4adf25053ce9749c8b88
Arthur <arthurvelasco4@gmail.com>
2025-05-29 23:10:21 -0300

    renderPlayer()

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
 const select = document.querySelector("#select");
 
-let pers = []
+let p1 = document.querySelector("#pl1");
+
+let p2 = document.querySelector("#pl2");
+
+let pers = ['ryu','ehonda','blanka','guile','ken','chunli','zangief','dhalsim'];
+
+let control = false;
+
+function renderPlayer(player){
+    if(control == false){
+        p1.setAttribute('src','src/images/gifs/'+player+'.gif');
+        control = true;
+    }
+    else{
+        p2.setAttribute('src','src/images/gifs/'+player+'.gif');
+        control = false;
+    }
+    
+}
+
+
