297c8d3b2ff85a1eca2b19264818ec53fe3eb1b0
Arthur <arthurvelasco4@gmail.com>
2025-05-31 01:27:15 -0300

    menu funcionando

--- src/script.js
+++ src/script.js
@@ -2,33 +2,33 @@ let p1 = document.querySelector("#pl1");
 
 let p2 = document.querySelector("#pl2");
 
-let select = document.querySelector("#container-select");
+let containerSelect = document.querySelector("#container-select");
 
-let pers = ['ryu','ehonda','blanka','guile','ken','chunli','zangief','dhalsim'];
+let pers = ['ryu', 'ehonda', 'blanka', 'guile', 'ken', 'chunli', 'zangief', 'dhalsim'];
+
+let selectList = document.querySelectorAll('.img-select');
 
 let control = false;
 
-function renderPlayer(player){
-    if(control == false){
-        p1.setAttribute('src','src/images/gifs/'+player+'.gif');
+function renderPlayer(player) {
+    if (control == false) {
+        p1.setAttribute('src', 'src/images/gifs/' + player + '.gif');
         control = true;
     }
-    else{
-        p2.setAttribute('src','src/images/gifs/'+player+'.gif');
+    else {
+        p2.setAttribute('src', 'src/images/gifs/' + player + '.gif');
         control = false;
     }
-    
+
 }
 
 
-function renderSelect(){
+function renderSelect() {
     let html = '';
-    for(let i = 0; i < pers.length; i++){
-        html += '<div class="select"><img src="src/images/menu/'+pers[i]+'.png" alt=""></div>';
-        
-        console.log(pers[i]);
+    for (let i = 0; i < pers.length; i++) {
+        html += '<div class="select"><img src="src/images/menu/' + pers[i] + '.png" alt="" class="img-select" onClick=renderPlayer("'+pers[i]+'")></div>';
     }
 
-    select.innerHTML = html;
+    containerSelect.innerHTML = html;
 
 }
